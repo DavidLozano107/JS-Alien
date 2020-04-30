afaf6869f60dd219bc793f7e95e821373c108c2d
David Lozano <davidlozano107@gmail.com>
2020-04-30 10:22:20 -0500

    Agregando la base de base del Juego

---
+++ src/init.js
@@ -0,0 +1,39 @@
+const config = {
+    width: 320*2,
+    height: 180*2,
+    parent: "container",
+    type: Phaser.AUTO,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: 500
+            }
+        }
+    }
+}
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image("alienOp", "./assets/Alien_3_Attacking0001.png");
+}
+function create() {
+    this.alienOp = this.physics.add.image(80, 100, "pajaro");
+    this.alienOp.setScale(2);
+    this.alienOp.flipX = false;
+    this.alienOp.setOrigin(0.5);  
+    // Fisicas
+    this.alienOp.setCollideWorldBounds(true);
+    this.alienOp.setBounce(0.3);
+    this.alienOp.setAcceleration(50, 0);
+
+}
+function update(time, delta) {
+    // this.alienOp.x++; 
+}
