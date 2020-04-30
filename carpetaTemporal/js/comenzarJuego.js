db4c7fb04bf25a10bc78546fea18ffb14adea7bb
Cristian Pérez <crialeperez1835@gmail.com>
2020-04-30 14:07:23 -0500

    carpetaTemporal Añadida

---
+++ carpetaTemporal/js/comenzarJuego.js
@@ -0,0 +1,10 @@
+
+function comenzarJuego(){
+	document.getElementById("imagen2").classList.add("active")
+	document.getElementById("abrirFondo2").classList.add("active")
+}
+
+document.getElementById("abrirFondo2").onclick = function(){
+	comenzarJuego();
+}
+
