5106c74f24951fa456319f64dea88f31c9e985c7
DavidLozano107 <53720016+DavidLozano107@users.noreply.github.com>
2020-05-03 09:54:24 -0500

    Seleccion de img 0.1

---
+++ src/scenes/world1.js
@@ -0,0 +1,15 @@
+//import SceneSelectPlyer from './sceneSelectPlyer.js';
+
+class Scene_play extends Phaser.Scene{
+    constructor(){
+        super({key: "Scene_play"});
+    }
+
+    create(){
+        
+    }
+
+}
+
+
+export default Scene_play;
