9ccb1d2b087e318d40ff795f9c22c3e7da91928e
DavidLozano107 <53720016+DavidLozano107@users.noreply.github.com>
2020-05-02 19:49:25 -0500

    agregue el media screen

---
+++ src/scenes/sceneSelectPlyer.js
@@ -0,0 +1,12 @@
+class SceneSelectPlayer extends Phaser.Scene{
+    constructor(){
+        super({key: "sceneSelectPlayer", active:true});
+    }
+
+    create(){
+        alert("Hello_perras");
+    }
+}
+
+
+export default sceneSelectPlayer;
